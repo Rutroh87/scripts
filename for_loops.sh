8f5c14637e410d2d2c51612c2bd0cef6181a9c6d
Joe <56459612+Rutroh87@users.noreply.github.com>
2020-10-27 15:55:35 -0700

    Create for_loops.sh

---
+++ for_loops.sh
@@ -0,0 +1,12 @@
+#!/bash/bin
+
+Define Variables
+for i in 1 2 3 4 5 6 7 8 9
+do
+echo "looping...number $i"
+done
+
+for s in California, Oregon, Washington, New York, Hawaii
+do
+echo "Looping... s is set to $s"
+done
